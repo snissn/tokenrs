631e2b1228f85eabea24bd45404ae761a5f1889e
mikers <mike@Mike.rs>
2018-10-20 18:54:33 -0400

    pg install

---
+++ pg.sh
@@ -0,0 +1,10 @@
+sudo apt-get install wget ca-certificates -y
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install postgresql-11 python3-postgresql -y
+sudo /etc/init.d/postgresql start
+
+
+sudo pip3 install psycopg2
